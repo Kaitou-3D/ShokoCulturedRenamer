2198b22df1b851918a9064b39f5c7281453303bf
Kaitou <kaitoukitsune@outlook.com>
2024-07-27 17:22:16 -0400

    init

---
+++ Shoko.Plugin.CulturedRenamer/CulturedRenamer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+namespace Renamer.CulturedRenamer;
+
+/// <summary>
+/// Cultured Renamer
+/// Target Folder Structure is based on type (OVA, Movies, Series) and Restricted > 18+
+/// </summary>
+public class CulturedRenamer
+{
+
+}
